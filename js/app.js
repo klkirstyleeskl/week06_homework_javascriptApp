eab9add695ec5ecdb951facf07c9cf9a1b2778cb
klkirstyleeskl <k.lees@btinternet.com>
2019-01-13 14:15:41 +0000

    adds cost to form

--- js/app.js
+++ js/app.js
@@ -8,22 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const handleFormSubmit = function (event){
   event.preventDefault();
   console.log(event.target.name.value);
-  console.log(event.target.artist.value)
-  // console.log(event.target.artwork.value)
+  console.log(event.target.artist.value);
+  console.log(event.target.cost.value);
+  // console.log(event.target.condition.value);
 
   const listItem = document.createElement('div')
 
-  const createListname = document.createElement('p')
+  const createListname = document.createElement('h2')
   createListname.textContent = `Name: ${event.target.name.value}`
   listItem.appendChild(createListname)
 
-  const createListartist = document.createElement('p')
+  const createListartist = document.createElement('h3')
   createListartist.textContent = `Artist: ${event.target.artist.value}`
   listItem.appendChild(createListartist)
 
-  // const createListimage = document.createElement('p')
-  // createListimage.textContent = `Cover Art: ${event.target.artwork.value}`
-  // listItem.appendChild(createListimage)
+  const createListcost = document.createElement('h4')
+  createListcost.textContent = `Cost £: ${event.target.cost.value}`
+  listItem.appendChild(createListcost)
+
+  // const createListcondition = document.createElement('p')
+  // createListcondition.textContent = `Conditon: ${event.target.condition.value}`
+  // listItem.appendChild(createListcondition)
+
 
   const list = document.querySelector('#record-collection-form')
   list.appendChild(listItem)
